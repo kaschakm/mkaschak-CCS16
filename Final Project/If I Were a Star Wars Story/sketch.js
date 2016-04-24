d6308964d61c81c20fd4dea25ca79b8da99e97f7
Morgan <mk770@UDAR-ITS-Helpdesk-MacBook-Air.local>
2016-04-23 20:37:00 -0400

    final project update

--- Final Project/If I Were a Star Wars Story/sketch.js
+++ Final Project/If I Were a Star Wars Story/sketch.js
@@ -1,7 +1,7 @@
 var words = [];
 var rl;
 var y;
-
+    
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textSize(20);
@@ -10,32 +10,29 @@ function setup() {
   
   //crawl from Return of the Jedi
   words = RiTa.tokenize("Luke Skywalker has returned to his home planet of Tatooine in an attempt to rescue his friend Han Solo from the clutches of the vile gangster Jabba the Hutt. Little does Luke know that the GALACTIC EMPIRE has secretly begun construction on a new armored space station even more powerful than the first dreaded Death Star. When completed, this ultimate weapon will spell certain doom for the small band of rebels struggling to restore freedom to the galaxy...");
-  
-  //speed that the words are replaced
-  //frameRate (0.1);
 }
-    
+
 function draw() {
- background(0);
+  background(0);
   translate (15, 20);
   scroll(0,y);
   y-=1;
   if(y < 0){
     y = height;
-}
+  }
 }
 
   //combine all of the text into a function
   function scroll(x,y) {
   //opening text
   fill(33, 131, 138);
-  text("A long time ago in a galaxy far, far away . . .",x,y);
+  text("A long time ago in a galaxy far, far away . . .",x,y, width, height);
   
   //plot text
   fill(250, 210, 10);
-  text(words.join(' '), x, y+300, width, height);
+  text(words.join(' '), x, y+100, width, height);
   }
-  
+
   function randomizeText() {
   //replace all verbs with random verbs
      for(var i=0; i < words.length; i++){
@@ -46,9 +43,11 @@ function draw() {
   //replace all adjectives with random adjectives
         else if (rl.isAdjective(words[i])){
           words[i] = rl.randomWord('jj');
-        }
-        }      
+          }  
+          }
+    setTimeout(randomizeText, 15000);
   }
+randomizeText();
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
--- Final Project/If I Were a TIme Traveller/sketch.js
+++ Final Project/If I Were a TIme Traveller/sketch.js
@@ -1,26 +1,25 @@
 var angle = 0.0;
 
 function setup() {
-  createCanvas (700,700);
-  background (140,140,0);
-}
-
-function draw() { 
+  createCanvas (windowWidth,windowHeight);
+  background (25);
+  textSize(32);
+  fill(0,160,0);
   translate (width/2, height/2);
+}
+function draw() { 
+  //random dates at the bottom of the screen
+  text(chance.date({string: true}),-80,height/2-3);
+
   rotate (angle);
 
   //interior lines
-  fill (0,0,75)
-  rect(frameCount % (width/3),100,30,10);
+  fill (0,0,170)
+  rect(frameCount % (width/3),0,40,10);
   
   //exterior circle
-  fill (75,0,0);
-  ellipse(10,300,70,40);
-
-  //loop of dates
-  textSize(15);
-  fill (0,75,0);
-  text(chance.date({string: true}),width/2,10);
+  fill (130,0,0);
+  ellipse(width/3,height/5,70,50);
 
   angle += 11.0;
 }  
